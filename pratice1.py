ac817a4595276bb57c31a58ffcb1dcae6bcbe501
uijin00 <tlsdmlwls21@naver.com>
2024-10-04 13:45:03 +0900

    new_branch1 1st Commit

--- pratice1.py
+++ pratice1.py
@@ -4,7 +4,14 @@ def add(x, y):
 def sub(x, y):
     return x - y
 
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
 n1 = float(input("첫 번째 정수를 입력하세요 : "))
-n2 = float(input("두 번째 정수를 입력하세요 : "))
+n2 = float(input("두 번째 정수(0을 제외한 정수)를 입력하세요 : "))
 
 print("덧셈 : ", add(n1, n2), ", " "뺄셈 : ", sub(n1, n2))
+print("곱셈 : ", mul(n1, n2), ", " "나눗셈 : ", div(n1, n2))
