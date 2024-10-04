bb228aa6ae0093c9873214aa367426623b0c1789
uijin00 <tlsdmlwls21@naver.com>
2024-10-04 13:39:09 +0900

    Initial Commit

---
+++ pratice1.py
@@ -0,0 +1,10 @@
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+n1 = float(input("첫 번째 정수를 입력하세요 : "))
+n2 = float(input("두 번째 정수를 입력하세요 : "))
+
+print("덧셈 : ", add(n1, n2), ", " "뺄셈 : ", sub(n1, n2))
